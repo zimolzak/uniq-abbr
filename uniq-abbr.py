1f4e9a6816e7ee86624601cd1b0d26cf0fa5d206
Andrew Zimolzak (Dell Core i5) <andyzimolzak@gmail.com>
2017-11-06 12:45:28 -0500

    Seems to be working as intended.

--- uniq-abbr.py
+++ uniq-abbr.py
@@ -27,14 +27,30 @@ P = T.split('\n')
 P.sort()
 #print(P)
 
+def string_heads(L):
+    H = []
+    for e in L:
+        while len(e) > 1:
+            e = e[0:-1]
+            H += [e]
+    return H
+
 D = {}
 for e in P:
+    print(e.upper())
     abbr = e[0]
-    while abbr in D.keys():
-        oldkey = abbr
-        oldval = D.pop(abbr)
-        newkey = oldkey + oldval[len(abbr)]
-        D[newkey] = oldval
+    forbidden_abbrs = list(D.keys()) + string_heads(D.keys())
+    while abbr in forbidden_abbrs:
+        if abbr in D.keys():
+            oldkey = abbr
+            oldval = D.pop(abbr)
+            newkey = oldkey + oldval[len(abbr)]
+            D[newkey] = oldval
+            print("extended", oldval, "to", newkey)
+        # else abbr was in the list of forbidden substrs, not real keys
         abbr += e[len(abbr)]
+        print("and extended", e, "to", abbr)
+        forbidden_abbrs = list(D.keys()) + string_heads(D.keys()) #update
     D[abbr] = e
+    print("good:", abbr)
 print(D)
